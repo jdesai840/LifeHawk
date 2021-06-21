77e25d921bc078f71558096dfa2287f5bad91aef
jdesai840 <69543549+jdesai840@users.noreply.github.com>
2021-06-21 00:33:07 -0700

    Add files via upload

---
+++ g4 Instance Python Code/rtmp.py
@@ -0,0 +1,121 @@
+import cv2 as cv
+import numpy as np
+import subprocess
+
+rtmp = r'rtmp://ec2-44-193-30-69.compute-1.amazonaws.com/live/drone'
+cap = cv.VideoCapture(0)
+size = (int(cap.get(cv.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)))
+sizeStr = str(size[0]) + 'x' + str(size[1])
+command = ['ffmpeg',
+    '-y', '-an',
+    '-f', 'rawvideo',
+    '-vcodec','rawvideo',
+    '-pix_fmt', 'bgr24',
+    '-s', sizeStr,
+    '-r', '25',
+    '-i', '-',
+    '-c:v', 'libx264',
+    '-pix_fmt', 'yuv420p',
+    '-preset', 'ultrafast',
+    '-f', 'flv',
+    rtmp]
+pipe = subprocess.Popen(command, shell=False, stdin=subprocess.PIPE
+)
+
+
+
+
+# cap = cv.VideoCapture(0)
+whT = 320
+confThreshold = 0.5
+nmsThreshold = 0.2
+trigger_confidence = None
+
+# LOAD MODEL
+# Coco Names
+classesFile = "YOLO_model/coco.names"
+classNames = []
+with open(classesFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+print(classNames)
+
+# Model Files
+modelConfiguration = "YOLO_model/yolov3-spp.cfg"
+modelWeights = "YOLO_model/yolov3-spp.weights"
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+
+# Function to find classes defined in model data
+def findObjects(outputs, img):
+    hT, wT, cT = img.shape
+    bbox = []
+    classIds = []
+    confs = []
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w, h = int(det[2] * wT), int(det[3] * hT)
+                x, y = int((det[0] * wT) - w / 2), int((det[1] * hT) - h / 2)
+                bbox.append([x, y, w, h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+
+    indices = cv.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+
+    # Drawing rectangles around detected objects
+    for i in indices:
+        i = i[0]
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        # print(x,y,w,h)
+        cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+        trigger_confidence = int(confs[0] * 100)
+        if trigger_confidence >= 98:
+            print("triggered")
+        cv.putText(img, f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',
+                   (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+
+
+# Video capture
+
+while cap.isOpened():
+    success, img = cap.read()
+    if success:
+        blob = cv.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+        net.setInput(blob)
+        layersNames = net.getLayerNames()
+        outputNames = [(layersNames[i[0] - 1]) for i in net.getUnconnectedOutLayers()]
+        outputs = net.forward(outputNames)
+        findObjects(outputs, img)
+        print(trigger_confidence)
+
+        cv.imshow('Image', img)
+        if cv.waitKey(1) & 0xFF == ord("d"):
+            break
+        pipe.stdin.write(img.tostring())
+
+cap.release()
+pipe.terminate()
+
+
+
+
+
+
+# Test code for images
+# img = cv.imread("images/chessy.jpeg")
+# blob = cv.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+# net.setInput(blob)
+# layersNames = net.getLayerNames()
+# outputNames = [(layersNames[i[0] - 1]) for i in net.getUnconnectedOutLayers()]
+# outputs = net.forward(outputNames)
+# findObjects(outputs, img)
+#
+# cv.imshow('Image', img)
+#
+# cv.waitKey(0)
